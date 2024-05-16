3424345651ea7721b97577dbd4f424988b9492ab
SweIsHere <angel.tribeno@utec.edu.pe>
2024-05-16 12:31:39 -0500

    Setteo

---
+++ src/main/java/org/example/conectatec/ConectatecApplication.java
@@ -0,0 +1,13 @@
+package org.example.conectatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectatecApplication.class, args);
+	}
+
+}
