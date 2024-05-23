2944fdcaf7ab6f99094178b7198353989dc7626a
ReatorG <renato.garcia@utec.edu.pe>
2024-05-22 20:24:48 -0500

    commit de prueba, ire creando las clases

--- src/main/java/org/example/conectatec/user/domain/User.java
+++ src/main/java/org/example/conectatec/user/domain/User.java
@@ -1,4 +1,31 @@
 package org.example.conectatec.user.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+
 }
