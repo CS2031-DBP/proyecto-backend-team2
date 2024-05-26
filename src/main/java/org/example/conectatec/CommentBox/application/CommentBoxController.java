13c72b8cc3098d96cfec091adbe8441f54fbb6d0
SweIsHere <angel.tribeno@utec.edu.pe>
2024-05-26 18:04:17 -0500

    Student 100%

--- src/main/java/org/example/conectatec/CommentBox/application/CommentBoxController.java
+++ src/main/java/org/example/conectatec/CommentBox/application/CommentBoxController.java
@@ -1,7 +1,54 @@
 package org.example.conectatec.CommentBox.application;
 
 
+import org.example.conectatec.CommentBox.domain.CommentBox;
+import org.example.conectatec.CommentBox.domain.CommentBoxService;
+import org.example.conectatec.studentPublications.domain.StudentPublications;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentBoxController {
 
+    @Autowired
+    private CommentBoxService commentBoxService;
+
+    @PostMapping
+    public ResponseEntity<CommentBox> createComment(@RequestBody CommentBox commentBox) {
+        CommentBox savedComment = commentBoxService.saveCommentBox(commentBox);
+        return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommentBox> getCommentById(@PathVariable Long id) {
+        Optional<CommentBox> commentBox = commentBoxService.findCommentBoxById(id);
+        return commentBox.map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/publication/{publicationId}")
+    public ResponseEntity<List<CommentBox>> getCommentsByPublication(@PathVariable Long publicationId) {
+        StudentPublications publication = new StudentPublications();
+        publication.setId(publicationId);  // por  ID
+        List<CommentBox> comments = commentBoxService.findCommentsByPublication(publication);
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long id) {
+        commentBoxService.deleteCommentBox(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentBox>> getAllComments() {
+        List<CommentBox> comments = commentBoxService.findAllComments();
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
 }
