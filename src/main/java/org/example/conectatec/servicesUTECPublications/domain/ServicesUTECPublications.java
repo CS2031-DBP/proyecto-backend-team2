7bc807ab376494807ce251034d5e81019cff0a9d
tamy <tamy.flores@utec.edu.pe>
2024-05-26 18:18:48 -0500

    tranquilossss

--- src/main/java/org/example/conectatec/servicesUTECPublications/domain/ServicesUTECPublications.java
+++ src/main/java/org/example/conectatec/servicesUTECPublications/domain/ServicesUTECPublications.java
@@ -1,4 +1,13 @@
 package org.example.conectatec.servicesUTECPublications.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+import org.example.conectatec.CommentBox.domain.CommentBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicesUTECPublications {
+    @OneToMany(mappedBy = "publication", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentBox> comments = new ArrayList<>();
 }
