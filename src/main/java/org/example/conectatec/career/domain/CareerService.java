8e56466b9bb40779f5302faf40ad3a2959787653
ReatorG <renato.garcia@utec.edu.pe>
2024-05-26 20:39:18 -0500

    Career Controller y Service terminados v1

--- src/main/java/org/example/conectatec/career/domain/CareerService.java
+++ src/main/java/org/example/conectatec/career/domain/CareerService.java
@@ -6,33 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CareerService {
 
-
     @Autowired
     private CareerRepository careerRepository;
 
-    public Career saveCareer(Career career) {
+    public Career getCareerById(Long id) {
+        return careerRepository.findById(id).get();
+    }
+
+    public List<Career> getAllCareers() {
+        return careerRepository.findAll();
+    }
+
+    public Career createCareer(Career career) {
         return careerRepository.save(career);
     }
 
-    public Optional<Career> findCareerById(Long id) {
-        return careerRepository.findById(id);
-    }
-
-    public Optional<Career> findCareerByName(String name) {
-        return careerRepository.findByName(name);
-    }
-
-    public void deleteCareer(Long id) {
-        careerRepository.deleteById(id);
-    }
-
-    public List<Career> findAllCareers() {
-        return careerRepository.findAll();
+    public void deleteCareerById(Long id) {
+        Career career = careerRepository.findById(id).get();
+        careerRepository.delete(career);
     }
 
 }
