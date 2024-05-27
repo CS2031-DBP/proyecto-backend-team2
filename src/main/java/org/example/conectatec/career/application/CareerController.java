1b6e0a21844f2a48dcf034a0be6582c6c39719f3
SweIsHere <angel.tribeno@utec.edu.pe>
2024-05-26 21:35:21 -0500

    Fixed Career endpoiint

--- src/main/java/org/example/conectatec/career/application/CareerController.java
+++ src/main/java/org/example/conectatec/career/application/CareerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/club")
+@RequestMapping("/career")
 public class CareerController {
 
     @Autowired
